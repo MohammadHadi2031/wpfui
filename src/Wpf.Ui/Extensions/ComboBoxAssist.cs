cda57727ba99dd71b8a838eb072c2142f20618bd
MohammadHadi2031 <mhadi.attarieh@gmail.com>
2023-08-30 19:40:53 +0330

    add place holder for combobox

---
+++ src/Wpf.Ui/Extensions/ComboBoxAssist.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Wpf.Ui.Extensions
+{
+    public static class ComboBoxAssist
+    {
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.RegisterAttached("Placeholder", typeof(string), typeof(ComboBoxAssist), new PropertyMetadata(""));
+
+        public static void SetPlaceholder(DependencyObject obj, string value) => obj.SetValue(PlaceholderProperty, value);
+
+        public static string GetPlaceholder(DependencyObject obj) => (string)obj.GetValue(PlaceholderProperty);
+    }
+}
